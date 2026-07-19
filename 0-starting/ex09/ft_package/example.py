c1dc32df81789ae6119eeb8a093a8d19b44da6d8
yukisato <yuki01377@gmail.com>
2026-07-19 16:21:43 +0900

    add ex09

---
+++ 0-starting/ex09/ft_package/example.py
@@ -0,0 +1,13 @@
+def count_in_list(lst: list, target: str) -> int:
+    count = 0
+    for x in lst:
+        if x == target:
+            count += 1
+    return count
+
+def main():
+    print(count_in_list(["toto", "tata", "toto"], "toto"))
+    print(count_in_list(["toto", "tata", "toto"], "tutu"))
+
+if __name__ == "__main__":
+    main();
