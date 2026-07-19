3dee6c5b9be1a95751db8719a4b01b93475a0444
yukisato <yuki01377@gmail.com>
2026-07-19 19:41:50 +0900

    add error handling and fix norminette

--- 1-array/ex00/give_bmi.py
+++ 1-array/ex00/give_bmi.py
@@ -9,12 +9,35 @@ def _apply_limit(bmi: int | float, limit: int) -> bool:
     return bmi > limit
 
 
-def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
-    res = np.frompyfunc(_give_bmi, 2, 1)(height, weight)
-    return list(res)
+def give_bmi(
+        height: list[int | float],
+        weight: list[int | float]) -> list[int | float]:
+    try:
+        assert height is not None and weight is not None, (
+                "the arguments are NoneType")
+        assert len(height) == len(weight), "the arguments are not same size"
+        for x in height:
+            assert isinstance(x, int) or isinstance(x, float), (
+                    "the arguments are not int or float")
+        for x in weight:
+            assert isinstance(x, int) or isinstance(x, float), (
+                    "the arguments are not int or float")
+        res = np.frompyfunc(_give_bmi, 2, 1)(height, weight)
+        return list(res)
+    except AssertionError as e:
+        print(f"{type(e).__name__}: {e}")
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
-    repeat_limit = [limit] * len(bmi)
-    res = np.frompyfunc(_apply_limit, 2, 1)(bmi, repeat_limit)
-    return list(res)
+    try:
+        assert bmi is not None and limit is not None, (
+                "the arguments are NoneType")
+        for x in bmi:
+            assert isinstance(x, int) or isinstance(x, float), (
+                    "the bmi are not int or float")
+        assert isinstance(limit, int), "the limit are not int"
+        repeat_limit = [limit] * len(bmi)
+        res = np.frompyfunc(_apply_limit, 2, 1)(bmi, repeat_limit)
+        return list(res)
+    except AssertionError as e:
+        print(f"{type(e).__name__}: {e}")
--- 1-array/ex00/tester.py
+++ 1-array/ex00/tester.py
@@ -1,8 +1,15 @@
 from give_bmi import give_bmi, apply_limit
 
-height = [2.71, 1.15, 1.71]
-weight = [165.3, 38.4, 68.3]
+# height = None
+# height = [2.71, 1.15, 1.71, 3]
+# height = [2.71, 1.15, "hello"]
+height = [2.71, 1.15]
+weight = [165.3, 38.4]
 
 bmi = give_bmi(height, weight)
 print(bmi, type(bmi))
-print(apply_limit(bmi, 26))
+# bmi = None
+# limit = "hoge"
+limit = 26
+# limit = None
+print(apply_limit(bmi, limit))
