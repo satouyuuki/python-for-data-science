98b8973176f00057d12fbf5c49818c18d204afd6
yukisato <yuki01377@gmail.com>
2026-07-19 23:39:53 +0900

    add error handlng

--- 1-array/ex02/load_image.py
+++ 1-array/ex02/load_image.py
@@ -3,7 +3,15 @@ from numpy import asarray
 
 
 def ft_load(path: str) -> list:
-    image = Image.open(path)
-    data = asarray(image)
-    print(f"The shape of image is: {data.shape}")
-    return data
+    try:
+        assert path is not None and isinstance(path, str), (
+                "the path are not str")
+        image = Image.open(path)
+        # print(type(image.format))
+        data = asarray(image)
+        print(f"The shape of image is: {data.shape}")
+        return data
+    # FileNotFoundError, PIL.UnidentifiedImageError
+    except (OSError, AssertionError) as e:
+        print(f"{type(e).__name__}: {e}")
+        return
--- 1-array/ex02/tester.py
+++ 1-array/ex02/tester.py
@@ -1,2 +1,7 @@
 from load_image import ft_load
-print(ft_load("landscape.jpg"))
+print(ft_load("empty.jpeg"))
+print(ft_load("player-small64.png"))
+# print(ft_load("result.txt"))
+# print(ft_load(""))
+# print(ft_load("dummy.jpg"))
+# print(ft_load("landscape.jpg"))
