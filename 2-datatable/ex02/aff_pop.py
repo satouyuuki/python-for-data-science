ccff9fe6056a63c9917caa8339e1edf0b58a3bdf
yukisato <yuki01377@gmail.com>
2026-07-22 00:48:44 +0900

    間隔調整

--- 2-datatable/ex02/aff_pop.py
+++ 2-datatable/ex02/aff_pop.py
@@ -2,6 +2,7 @@ from load_csv import load
 import seaborn as sns
 from matplotlib import pyplot
 # import pandas as pd
+# import numpy as np
 
 
 def population(countries: list[str]) -> None:
@@ -12,7 +13,7 @@ def population(countries: list[str]) -> None:
         filter_df = df[df.index.isin(idx_list)]
         print(filter_df.index)
         df_flipped = filter_df.T
-        df_flipped = df_flipped.set_index('country')
+        # df_flipped = df_flipped.set_index('country')
         df_flipped = df_flipped.replace({'M': ''}, regex=True).astype(float)
         print(df_flipped)
     except AssertionError as e:
@@ -20,10 +21,23 @@ def population(countries: list[str]) -> None:
     except KeyError as e:
         print(f"{type(e).__name__}: {e}")
     else:
-        ax = sns.lineplot(data=df_flipped)
+        my_colors = ['red', 'blue']
+        ax = sns.lineplot(data=df_flipped[countries], palette=my_colors)
+        # ax = sns.lineplot(data=df_flipped, palette=my_colors)
+        years = df.loc['country'].tolist()
+        print(years)
+        tick_indices = range(0, len(years), 40)
+        tick_labels = [years[i] for i in tick_indices]
+        ax.set_xticks(tick_indices, labels=tick_labels)
         ax.set_title("Population Projections")
         ax.set_xlabel("Year")
         ax.set_ylabel("Population")
+        ax.legend(title='')
+        handles, labels = ax.get_legend_handles_labels()
+        print(handles, labels)
+        ax.legend(handles=handles[-2:], labels=labels[-2:])
+        # ax.legend(labels=countries)
+        # ax.set_xticks(range(1800, 2050, 40))
         pyplot.show()
 
 
