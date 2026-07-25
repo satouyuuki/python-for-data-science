cbfafaa7ab820a48a434c59c8e729b0c7c9bce93
yukisato <yuki01377@gmail.com>
2026-07-25 12:28:29 +0900

    add ex02

--- 3-oop/ex02/DiamondTrap.py
+++ 3-oop/ex02/DiamondTrap.py
@@ -2,5 +2,17 @@ from S1E7 import Baratheon, Lannister
 
 
 class King(Baratheon, Lannister):
-    # your code here
-    pass
+    def __init__(self, first_name, is_alive=True):
+        super().__init__(first_name, is_alive)
+
+    def get_eyes(self):
+        return self.eyes
+
+    def set_eyes(self, val):
+        self.eyes = val
+
+    def get_hairs(self):
+        return self.hairs
+
+    def set_hairs(self, val):
+        self.hairs = val
