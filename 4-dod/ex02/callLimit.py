afcdfbbc8cd53ed17bd23f2aeb18b9a1889e24d1
yukisato <yuki01377@gmail.com>
2026-07-26 10:20:52 +0900

    init ex02

---
+++ 4-dod/ex02/callLimit.py
@@ -0,0 +1,8 @@
+def callLimit(limit: int):
+    count = 0
+    def callLimiter(function):
+        def limit_function(*args, **kwds):
+         # your code here
+            pass
+        pass
+    pass
