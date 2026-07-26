73f13b456685bb82f6b6378cc6c2f1b96d5aa986
yukisato <yuki01377@gmail.com>
2026-07-26 10:50:40 +0900

    add ex02

--- 4-dod/ex02/callLimit.py
+++ 4-dod/ex02/callLimit.py
@@ -1,8 +1,13 @@
 def callLimit(limit: int):
     count = 0
+
     def callLimiter(function):
         def limit_function(*args, **kwds):
-         # your code here
-            pass
-        pass
-    pass
+            nonlocal count
+            count += 1
+            if count > limit:
+                print(f"Error: {function} call too many times")
+            else:
+                function()
+        return limit_function
+    return callLimiter
